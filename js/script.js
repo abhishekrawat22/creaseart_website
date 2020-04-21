5d483e40a856dd6a8518435b8be8261b7590d5d3
abhishekrawat22 <abhishekrawat5743@gmail.com>
2020-04-21 13:21:14 +0530

    Updated responsive home page with login signup side bar

--- js/script.js
+++ js/script.js
@@ -1,44 +1,55 @@
 
 
-function tableContentPanel(evt, contentId) {
+// function tableContentPanel(evt, contentId) {
 
-  let i, tableTabcontent, tableTabLinks;
+//   let i, tableTabcontent, tableTabLinks;
 
-  tableTabcontent = document.getElementsByClassName("tableTab-content");
+//   tableTabcontent = document.getElementsByClassName("tableTab-content");
 
-  for (i = 0; i < tableTabcontent.length; i++) {
+//   for (i = 0; i < tableTabcontent.length; i++) {
 
-    tableTabcontent[i].style.display = "none";
+//     tableTabcontent[i].style.display = "none";
 
-  }
+//   }
 
-  tableTabLinks = document.getElementsByClassName("tableTabLinks");
+//   tableTabLinks = document.getElementsByClassName("tableTabLinks");
 
-  for (i = 0; i < tableTabLinks.length; i++) {
+//   for (i = 0; i < tableTabLinks.length; i++) {
 
-    tableTabLinks[i].className = tableTabLinks[i].className.replace(" active", "");
+//     tableTabLinks[i].className = tableTabLinks[i].className.replace(" active", "");
 
-  }
+//   }
 
-  document.getElementById(contentId).style.display = "block";
+//   document.getElementById(contentId).style.display = "block";
 
-  evt.currentTarget.className += " active";
+//   evt.currentTarget.className += " active";
 
-  let itemPrice = document.getElementsByClassName('item_price')[0];
-  let itemPriceTotal = document.getElementsByClassName('item_price_total')[0];
-  let priceTotal = document.getElementsByClassName('priceTotal')[0];
+//   let itemPrice = document.getElementsByClassName('item_price')[0];
+//   let itemPriceTotal = document.getElementsByClassName('item_price_total')[0];
+//   let priceTotal = document.getElementsByClassName('priceTotal')[0];
 
-  const calculate = () => {
-    priceTotal[0].innerHTML = parseInt(itemPrice.value) * parseInt(itemPriceTotal.value);
-  }
+//   const calculate = () => {
+//     priceTotal[0].innerHTML = parseInt(itemPrice.value) * parseInt(itemPriceTotal.value);
+//   }
 
+// }
+
+// For login side bar
+function openLogform() {
+  document.getElementById("login").style.width = "25%";
+  document.getElementById("signup").style.width = "0%";
 }
 
-// For mobile menu
-function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+function closeLogform() {
+  document.getElementById("login").style.width = "0%";
 }
 
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+// For signup side bar
+function openSignform() {
+  document.getElementById("signup").style.width = "25%";
+  document.getElementById("login").style.width = "0%";
+}
+
+function closeSignform() {
+  document.getElementById("signup").style.width = "0%";
 }
